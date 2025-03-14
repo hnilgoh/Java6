ef3bd2af3456eca10c35cb7bd5290942be2e6c53
hnilgoh <linhnttpd10059@gmail.com>
2025-03-14 16:53:24 +0700

    java6

---
+++ src/main/java/com/example/java6/model/Order.java
@@ -0,0 +1,30 @@
+package com.example.java6.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Orders")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "Createdate")
+    private Date createDate = new Date();
+
+    @ManyToOne
+    @JoinColumn(name = "Username")
+    private Account account;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetail> orderDetails;
+}
